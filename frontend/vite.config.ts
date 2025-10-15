27f555c32f4f328271e607bc2accf896e5ded958
Juan Vidal <juanmvg2003@gmail.com>
2025-10-14 19:13:17 -0500

    fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  preview: {
-    allowedHosts: [process.env.VITE_ALLOWED_HOST as string],
-    port: 8080,
-    host: true
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const allowedHost = env.VITE_ALLOWED_HOST;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    preview: {
+      allowedHosts: [allowedHost],
+      port: 8080,
+      host: true
+    }
   }
 })
