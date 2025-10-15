69eff3cc60c2c090d0b71444ae33542db65b8ede
Juan Vidal <juanmvg2003@gmail.com>
2025-10-14 19:05:45 -0500

    add host to vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+  preview: {
+    allowedHosts: [process.env.VITE_ALLOWED_HOST as string],
+    port: 8080,
+    host: true
+  }
 })
